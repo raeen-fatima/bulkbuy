911f746db92bc57796259b0d341b4236ce9077c8
Thatcoderboy01 <thatcoderboy01@gmail.com>
2025-07-25 22:05:59 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold text-center">"A hackathon isn’t just about code — it’s about turning ideas into impact. I’m proud to have you by my side on this journey. Let’s build something meaningful together! 🚀🔥"</h1>
+      </div>
+  )
+}
+
+export default App;
