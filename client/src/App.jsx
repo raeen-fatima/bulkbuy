5764e0e8bc31a214c9d4613116d64d47bb1a5038
raeen-fatima <raeenfatimahere@gmail.com>
2025-07-27 21:09:35 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Services from './pages/Services';
-import Navbar from './components/Landing/Navbar';
-import Footer from './components/Landing/Footer';
-import DealDetails from './components/services/DealDetails';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Register';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Services from "./pages/Services";
+import Navbar from "./components/Landing/Navbar";
+import Footer from "./components/Landing/Footer";
+import DealDetails from "./components/services/DealDetails";
+import Summary from "./components/services/Summary";
+import Success from "./components/services/Success";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Register";
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
         <Route path="/deals/:id" element={<DealDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/buy/:id" element={<Summary />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
       <Footer />
     </>
@@ -25,5 +29,3 @@ const App = () => {
 };
 
 export default App;
-
- 
